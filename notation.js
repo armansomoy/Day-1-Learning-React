f8855fbf45892779c58caa6e3697c26722381b29
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2024-03-06 00:16:59 +0600

    notation, session storage

---
+++ notation.js
@@ -0,0 +1,20 @@
+const person = {
+    name: 'red',
+    prefession: 'police',
+    age: '23',
+    25: 'id',
+    address: 'jurain'
+
+}
+
+// dot notation 
+const prof1 = person.prefession;
+
+// bracket notation 
+const prof2 = person["prefession"];
+const pName = 'proefession';
+const prof3 = person[pName];
+
+const idNum = person[25];
+console.log(idNum)
+console.log(prof2);
